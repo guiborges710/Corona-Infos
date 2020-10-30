d138281c19776f6a95cb31183e40c78a8e92ce05
guilherme.martinelli <guiulherme.martinelli@MSAOC02Z961FLVCE.local>
2020-10-30 12:02:39 -0300

    commit

---
+++ Corona Infos/Pods/Target Support Files/Pods-Corona Infos-Corona InfosUITests/Pods-Corona Infos-Corona InfosUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Corona_Infos_Corona_InfosUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Corona_Infos_Corona_InfosUITests
+@end
---
+++ Corona Infos/Pods/Target Support Files/Pods-Corona Infos/Pods-Corona Infos-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Corona_Infos : NSObject
+@end
+@implementation PodsDummy_Pods_Corona_Infos
+@end
